9af89865422abec81a2a2e3c71ea87ac3875de1d
Junhao Dong <junhao.dong96@gmail.com>
2018-09-17 22:45:38 +0800

    Remove theme from init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,7 +22,3 @@
 
 ;; Misc
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-
-(custom-set-variables
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
-(custom-set-faces)
