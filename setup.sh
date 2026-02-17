2971a60f1495d654d85e0f5fa2518efc9a3e10b4
Junhao Dong <junhao@fb.com>
2026-02-17 14:11:11 -0500

    Update setup.sh to setup custom MacOS keybindings

--- setup.sh
+++ setup.sh
@@ -9,9 +9,11 @@ echo "Installing Homebrew..."
 if [[ $(command -v brew) == "" ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+echo "Done"
 
 echo "Installing zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Done"
 
 echo "Copying bash, zsh, emacs, dircolors..."
 cp $SCRIPT_HOME/bashrc ~/.bashrc
@@ -19,21 +21,29 @@ cp $SCRIPT_HOME/zshrc ~/.zshrc
 cp -r $SCRIPT_HOME/oh-my-zsh/custom/ ~/.oh-my-zsh/custom
 cp -r $SCRIPT_HOME/emacs.d/ ~/.emacs.d
 cp $SCRIPT_HOME/dircolors ~/.dircolors
+echo "Done"
 
 echo "Setup Solarized emacs..."
 # Optionally customize to dark theme
 git clone https://github.com/sellout/emacs-color-theme-solarized.git
 mv emacs-color-theme-solarized/* $SCRIPT_HOME/emacs.d/themes/
 rm -r emacs-color-theme-solarized
+echo "Done"
 
-echo "Setup Solarized dircolors"
+echo "Setup Solarized dircolors..."
 if [[ $(command -v gdircolors) == "" ]]; then
     brew install coreutils
 fi
 gdircolors
+echo "Done"
 
-echo "Setup Solarized terminal"
-echo "See https://github.com/tomislav/osx-terminal.app-colors-solarized"
+echo "Setup Solarized terminal..."
+echo "Skipped, do it manually, see https://github.com/tomislav/osx-terminal.app-colors-solarized"
+echo "Done"
 
-echo "Done."
+echo "Setup custom MacOS keybindings..."
+mkdir -p ~/Library/KeyBindings/
+cp $SCRIPT_HOME/DefaultKeyBinding.dict ~/Library/KeyBindings/
+echo "Done"
 
+echo "Finished running script."
